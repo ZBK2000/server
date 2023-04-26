8cb26270a38785b1211882ee5fef87f39df1729b
Zupko Balazs <you@example.com>
2023-04-26 10:43:38 +0200

    error solve

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "../../../../public/images");
-    cb(null, "public/images")
+    //cb(null, "public/images")
   },
   filename: function (req, file, cb) {
     cb(
